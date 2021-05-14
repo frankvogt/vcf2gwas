355335b8cc8e6f51c720306e0f79ded9acc6c297
Frank Vogt <55954992+frankvogt@users.noreply.github.com>
2021-05-14 16:44:22 +0200

    updates

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ setup(
     entry_points={
         'console_scripts': [
             'vcf2gwas=vcf2gwas.__main__:main',
-            'install_vcf2gwas=vcf2gwas.install:main'
-            #'test_vcf2gwas=run_test:main'
+            'install_vcf2gwas=vcf2gwas.install:main',
+            'test_vcf2gwas=run_test:main'
         ]
     },
     install_requires=requirements,
