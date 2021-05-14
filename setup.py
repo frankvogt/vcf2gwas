3f21b437c89cbf474147afddf7c9bd3529bf53a6
Frank Vogt <55954992+frankvogt@users.noreply.github.com>
2021-05-14 16:35:04 +0200

    updated

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup
 
 requirements = [
     # package requirements go here
-    'umap-learn'
 ]
 
 setup(
@@ -19,8 +18,8 @@ setup(
     entry_points={
         'console_scripts': [
             'vcf2gwas=vcf2gwas.__main__:main',
-            'install_vcf2gwas=vcf2gwas.install:main',
-            'test_vcf2gwas=run_test:main'
+            'install_vcf2gwas=vcf2gwas.install:main'
+            #'test_vcf2gwas=run_test:main'
         ]
     },
     install_requires=requirements,
