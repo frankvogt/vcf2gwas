c31ad363bedea8ab74e8b0245ef94be201d6aace
Frank Vogt <55954992+frankvogt@users.noreply.github.com>
2021-05-14 17:12:43 +0200

    updated

--- conda.recipe/run_test.py
+++ conda.recipe/run_test.py
@@ -1,15 +1,3 @@
-try:
-    import umap
-except:
-    subprocess.run(["pip", "install", "umap-learn"])
-
 from vcf2gwas.utils import *
 from vcf2gwas.parsing import *
 
-argvals = "-h"
-
-def main():
-    Parser(argvals)
-
-
-main()
