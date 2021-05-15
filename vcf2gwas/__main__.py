053662278a82e5b1ecaaef6a2761a415372b7b4d
Frank Vogt <55954992+frankvogt@users.noreply.github.com>
2021-05-15 17:43:37 +0200

    updated

--- vcf2gwas/__main__.py
+++ vcf2gwas/__main__.py
@@ -10,22 +10,17 @@ from vcf2gwas.install import main as installer
 argvals = None
 
 def main(argvals=argvals):
-    #try:
-    #    import umap
-    #except:
-    #    subprocess.run(["pip", "install", "umap-learn"])
-    #if "umap-learn" not in str(subprocess.run(["conda", "list"], capture_output=True)):
+    #string = str(subprocess.run(["conda", "list"], capture_output=True))
+    #modules = ["gemma", "plink", "bcftools"]
+    #if any(x not in string for x in modules):
     #    installer()
-    string = str(subprocess.run(["conda", "list"], capture_output=True))
-    modules = ["gemma", "plink", "bcftools"]
-    if any(x not in string for x in modules):
-        installer()
         #subprocess.run(["conda", "install", "-c", "bioconda", "bcftools==1.12*"])
         #subprocess.run(["conda", "install", "-c", "bioconda", "plink==1.90*"])
         #subprocess.run(["conda", "install", "-c", "bioconda", "gemma==0.98.3"])
+    
     print("\nvcf2gwas v0.5 \n")
     print("Initialising..\n")
-    Parser(argvals)
+    P = Parser(argvals)
     args = sys.argv[1:]
     #args.insert(0, 'conda')
     #args.insert(1, 'run')
@@ -36,6 +31,12 @@ def main(argvals=argvals):
     #args.insert(6, os.path.join(os.path.dirname(__file__), 'starter.py'))
     args.insert(0, 'python')
     args.insert(1, os.path.join(os.path.dirname(__file__), 'starter.py'))
+    
+    geno = P.set_geno()
+    if geno == "test":
+        installer()
+        args = f'python {os.path.join(os.path.dirname(__file__), 'starter.py')} -v input/example.vcf.gz -pf input/example.csv -p 1 -lmm'.split()
+    
     subprocess.run(args)
 
 if __name__ == '__main__':
--- vcf2gwas/install.py
+++ vcf2gwas/install.py
@@ -3,16 +3,17 @@ import os
 import shutil
 
 def main():
-    print("Checking environment.. \nUpdating dependencies")
-    env_name = os.environ["CONDA_DEFAULT_ENV"]
-    subprocess.run(["conda", "env", "update", "-n", env_name, "-f", os.path.join(os.path.dirname(__file__), 'environment.yml')])
+    #print("Checking environment.. \nUpdating dependencies..")
+    #env_name = os.environ["CONDA_DEFAULT_ENV"]
+    #subprocess.run(["conda", "env", "update", "-n", env_name, "-f", os.path.join(os.path.dirname(__file__), 'environment.yml')])
     #try:
     #    #subprocess.run(["pip", "install", "umap-learn"])
     #    subprocess.run(["conda", "env", "create", "-f",
     #        os.path.join(os.path.dirname(__file__), 'environment.yml')])
     #except Exception as e:
     #    print(e)
-    print("Environment is now up to date \nCopying example input files to current working directory..")
+    #print("Environment is now up to date")
+    print("Copying example input files to current working directory..")
     try:
         os.mkdir("input")
     except:
@@ -23,4 +24,4 @@ def main():
     print("Copying README and LICENSE files..")
     shutil.copy(os.path.join(os.path.dirname(__file__), 'README.md'), 'README.md')
     shutil.copy(os.path.join(os.path.dirname(__file__), 'LICENSE'), 'LICENSE')
-    print("Installation complete\n")
+    #print("Installation complete\n")
