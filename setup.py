91c7bc9e19bf9ddec50a98ed6d3aab2132afb2dc
Frank Vogt <55954992+frankvogt@users.noreply.github.com>
2021-05-16 21:48:04 +0200

    updated

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     url='https://github.com/frankvogt/vcf2gwas',
     packages=['vcf2gwas'],
     package_dir={'vcf2gwas': 'vcf2gwas'},
-    package_data={'vcf2gwas': ['*.yml', 'input/example.*', 'README.*', 'LIC*']},
+    package_data={'vcf2gwas': ['*.yml', 'input/example.*', 'README.*', 'LIC*', 'MAN*']},
     entry_points={
         'console_scripts': [
             'vcf2gwas=vcf2gwas.__main__:main'
