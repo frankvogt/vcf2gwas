02655a13b801f092b3add51dc0b5e098cee91901
Frank Vogt <55954992+frankvogt@users.noreply.github.com>
2021-10-14 01:04:59 +0200

    updated

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requirements = [
 
 setup(
     name='vcf2gwas',
-    version='0.7.5',
+    version='0.7.6',
     description="Python API for comprehensive GWAS analysis using GEMMA",
     license="GNUv3",
     author="Frank Vogt",
