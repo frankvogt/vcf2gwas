e1192b9200f95c495b5a07fec8673d08f510335b
Frank Vogt <frvogt@gmail.com>
2023-05-25 23:09:02 +0200

    v0.8.9

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='vcf2gwas',
+    version='0.8.9',
+    description="Python API for comprehensive GWAS analysis using GEMMA",
+    license="GNUv3",
+    author="Frank Vogt",
+    url='https://github.com/frankvogt/vcf2gwas',
+)
