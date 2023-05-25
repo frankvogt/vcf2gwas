7318c6b89a7e84bbeab769f876bbd0534f97a59b
Frank Vogt <frvogt@gmail.com>
2023-05-25 23:34:57 +0200

    v0.8.9

--- setup.py
+++ setup.py
@@ -7,4 +7,7 @@ setup(
     license="GNUv3",
     author="Frank Vogt",
     url='https://github.com/frankvogt/vcf2gwas',
+    packages=['vcf2gwas'],
+    package_dir={'vcf2gwas': 'vcf2gwas'},
+    package_data={'vcf2gwas': ['*.yml', 'input/example.*', 'GFF_files/GFF*', 'README.*', 'LIC*']}
 )
